c3811dc98f2bc2afa132bd267acb176d7c579f82
Pierre H. Lehnen <Hudell@users.noreply.github.com>
2017-08-22 12:23:04 -0300

    Update GameTweaker.js

--- GameTweaker.js
+++ GameTweaker.js
@@ -5,7 +5,7 @@
   }
 
   if (UpdateManager.currentVersion > 0.201) {
-    console.log('[GameTweaker] This mod may be outdaded.');
+    console.log('[GameTweaker] This mod may be outdated.');
   }
 
   function GameTweakerContent() {
